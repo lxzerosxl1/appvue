66205361485dc6abb27ec7de41338fddfed5780f
lxzerosxl1 <compuoperador2@gmail.com>
2020-07-27 17:32:59 -0500

    hoy dia

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-// const app = new Vue({
-//     el: '#app',
-//     data: {
-//         titulo: 'Hola mundo',
-//         frutas: [
-//             {nombre:'Pera', cantidad:10},
-//             {nombre: 'Manzana', cantidad:4},
-//             {nombre: 'mandarina', cantidad:46}
-//         ],
-//         nuevaFruta: ''
-//     },
-//     methods: {
-//         agregarFruta () {
-//             this.frutas.push({
-//                 nombre: this.nuevaFruta, cantidad: 0
-//             });
-//             this.nuevaFruta = '';
-//         }
-//     }
-// })
-
-const calc = new Vue({
-    el:'#suma',
+const app = new Vue({
+    el: '#app',
     data: {
-        numeros: [
-            {num : 0},{num : 1},{num : 2},{num : 3},{num : 4},{num : 5},
-            {num : 6},{num : 7},{num : 8},{num : 9}
-        ],
-        suma : '',
-        resta : '',
-        multi : '',
-        divi : ''
+        titulo : 'GYM con Vue',
+        tareas: [],
+        nuevaTarea: ''
     },
-    methods : {
-        agregarNumero () {
-            
+    methods: {
+        agregarTarea() {
+            // console.log('Diste Click', this.nuevaTarea);
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            console.log(this.tareas)
+            this.nuevaTarea = '';
         }
-    }
-})
+    },
+});
