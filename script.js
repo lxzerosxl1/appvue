649c573206f85f305742c96f571b61ea13578626
lxzerosxl1 <compuoperador2@gmail.com>
2020-07-26 22:22:46 -0500

    prueba calculadora

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const app = new Vue({
-    el: '#app',
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         titulo: 'Hola mundo',
+//         frutas: [
+//             {nombre:'Pera', cantidad:10},
+//             {nombre: 'Manzana', cantidad:4},
+//             {nombre: 'mandarina', cantidad:46}
+//         ],
+//         nuevaFruta: ''
+//     },
+//     methods: {
+//         agregarFruta () {
+//             this.frutas.push({
+//                 nombre: this.nuevaFruta, cantidad: 0
+//             });
+//             this.nuevaFruta = '';
+//         }
+//     }
+// })
+
+const calc = new Vue({
+    el:'#suma',
     data: {
-        titulo: 'Hola mundo',
-        frutas: [
-            {nombre:'Pera', cantidad:10},
-            {nombre: 'Manzana', cantidad:4},
-            {nombre: 'mandarina', cantidad:46}
+        numeros: [
+            {num : 0},{num : 1},{num : 2},{num : 3},{num : 4},{num : 5},
+            {num : 6},{num : 7},{num : 8},{num : 9}
         ],
-        nuevaFruta: ''
+        suma : '',
+        resta : '',
+        multi : '',
+        divi : ''
     },
-    methods: {
-        agregarFruta () {
-            this.frutas.push({
-                nombre: this.nuevaFruta, cantidad: 0
-            });
-            this.nuevaFruta = '';
+    methods : {
+        agregarNumero () {
+            
         }
     }
 })
